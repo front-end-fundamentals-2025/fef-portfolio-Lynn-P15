5f15a8282304e837c84c3e906c0a5f99aaf89d27
Lynn <pely24wt@student.ju.se>
2025-02-21 16:43:30 +0100

    light mode

--- java/event.js
+++ java/event.js
@@ -1,16 +1,13 @@
 const darkModeButton = document.getElementById("dark-mode-button");
+const lightModeButton = document.getElementById("light-mode-button");
+
 const bodyElement = document.querySelector("body");
 
 darkModeButton.addEventListener("click", function (event) {
   bodyElement.style.backgroundColor = "#000000";
   bodyElement.style.color = "#ffffff";
 });
-
-// work in progress
-
-// const darkModeButtonWork = document.getElementById("dark-mode-button-work");
-
-// darkModeButtonWork.addEventListener("click", function (event) {
-//   bodyElement.style.backgroundColor = "#000000";
-//   bodyElement.style.color = "#ffffff";
-// });
+lightModeButton.addEventListener("click", function (event) {
+  bodyElement.style.backgroundColor = "#ffffff";
+  bodyElement.style.color = "#000000";
+});
