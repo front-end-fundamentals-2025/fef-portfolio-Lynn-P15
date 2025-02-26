b625c561abb03d4273a1b7d28e57b1bc7b393699
Lynn <pely24wt@student.ju.se>
2025-02-26 04:31:12 +0100

    memory on the light and dark colour

--- java/event.js
+++ java/event.js
@@ -3,11 +3,30 @@ const lightModeButton = document.getElementById("light-mode-button");
 
 const bodyElement = document.querySelector("body");
 
-darkModeButton.addEventListener("click", function (event) {
+if (localStorage.bColour) {
+  bodyElement.style.backgroundColor = localStorage.bColour;
+} else {
   bodyElement.style.backgroundColor = "#000000";
+}
+if (localStorage.tColour) {
+  bodyElement.style.color = localStorage.tColour;
+} else {
   bodyElement.style.color = "#ffffff";
+}
+
+darkModeButton.addEventListener("click", function (event) {
+  let bColour = "#000000";
+  let tColour = "#ffffff";
+  bodyElement.style.backgroundColor = bColour;
+  bodyElement.style.color = tColour;
+  localStorage.bColour = bColour;
+  localStorage.tColour = tColour;
 });
 lightModeButton.addEventListener("click", function (event) {
-  bodyElement.style.backgroundColor = "#ffffff";
-  bodyElement.style.color = "#000000";
+  let bColour = "#ffffff";
+  let tColour = "#000000";
+  bodyElement.style.backgroundColor = bColour;
+  bodyElement.style.color = tColour;
+  localStorage.bColour = bColour;
+  localStorage.tColour = tColour;
 });
